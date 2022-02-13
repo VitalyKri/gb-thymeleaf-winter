5dfbed9f2f6a6503ad2798e50912cfcd782cefb2
vitaly <VitalyKri123@gmail.com>
2022-02-13 19:05:06 +0300

    add homework8

---
+++ src/main/java/ru/gb/gbthymeleafwinter/dao/CartDao.java
@@ -0,0 +1,18 @@
+package ru.gb.gbthymeleafwinter.dao;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.gb.gbthymeleafwinter.entity.Cart;
+import ru.gb.gbthymeleafwinter.entity.Product;
+import ru.gb.gbthymeleafwinter.entity.enums.Status;
+
+import java.util.List;
+
+public interface CartDao extends JpaRepository<Cart,Long> {
+
+    List<Cart> findAllByStatus(Status status);
+    List<Cart> findAllByStatus(Status status, Pageable pageable);
+    List<Cart> findAllByStatus(Status status, Sort sort);
+
+}
