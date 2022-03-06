367285765b4b0d43a5a320a850af2b328ec166af
vitaly <VitalyKri123@gmail.com>
2022-03-06 15:12:45 +0300

    add lesson12

--- src/main/java/ru/gb/gbthymeleafwinter/controller/CartController.java
+++ src/main/java/ru/gb/gbthymeleafwinter/controller/CartController.java
@@ -3,6 +3,7 @@ package ru.gb.gbthymeleafwinter.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/cart")
+//TODO нужно добавить роли для корзины
 public class CartController {
 
     private final CartService cartService;
@@ -25,6 +27,7 @@ public class CartController {
 
 
     @GetMapping()
+    @PreAuthorize("hasAuthority('cart.read')")
     public String showForm(Model model, @RequestParam(name = "id", required = false) Long id) {
 
         if (id==null){
@@ -39,6 +42,7 @@ public class CartController {
 
 
     @GetMapping("/addProduct")
+    @PreAuthorize("hasAuthority('cart.update')")
     public String addProduct(@RequestParam(name = "id") Long id, HttpServletRequest request) {
 
         Cart cart = cartService.findById(1L);
@@ -55,6 +59,7 @@ public class CartController {
 
 
     @GetMapping("/deleteProduct")
+    @PreAuthorize("hasAuthority('cart.update')")
     public String deleteProduct(@RequestParam(name = "id") Long id, HttpServletRequest request) {
 
         Cart cart = cartService.findById(1L);
